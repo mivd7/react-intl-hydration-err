e37321eae04aa1dea4adea03f46de32263484c1b
Max van Deurzen <max@oberon.nl>
2023-07-20 14:04:13 +0200

    init commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'nl'],
+    defaultLocale: 'nl',
+
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+import { IntlProvider } from 'react-intl'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const { locale, defaultLocale } = useRouter();
+
+return (
+  <IntlProvider locale={locale as string} defaultLocale={defaultLocale} messages={pageProps.intlMessages}>
+    <Component {...pageProps} />
+  </IntlProvider>)
 }
